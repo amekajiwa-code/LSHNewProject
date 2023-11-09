334fe13066113685c9967f7f7e6aba3eb8c446c9
songha <paperstory10@naver.com>
2023-11-09 19:09:24 +0900

    루키스 게임서버

---
+++ GameServer/GameServer/GameServer.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+
+#include <thread>
+
+void HelloThread()
+{
+	std::cout << "Hello thread" << std::endl;
+}
+
+int main()
+{
+	std::thread t(HelloThread);
+
+	std::cout << "Hello main" << std::endl;
+
+	t.join();
+}
