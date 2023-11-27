49b7a980889f6e7002f3f995e2334742e40e8a85
songha <paperstory10@naver.com>
2023-11-27 19:21:33 +0900

    qqa

--- GameServer/GameServer/GameServer.cpp
+++ GameServer/GameServer/GameServer.cpp
@@ -70,21 +70,55 @@ void ThreadRead()
 	}
 }
 
+//int main()
+//{
+//	for (int32 i = 0; i < 5; ++i)
+//	{
+//		cout << i << endl;
+//	}
+//	/*for (int32 i = 0; i < 2; ++i)
+//	{
+//		GThreadManager->Launch(ThreadWrite);
+//	}
+//
+//	for (int32 i = 0; i < 5; ++i)
+//	{
+//		GThreadManager->Launch(ThreadRead);
+//	}
+//
+//	GThreadManager->Join();*/
+//}
+
+#include <iostream>
+#include <mutex>
+#include <shared_mutex>
+
+std::shared_mutex mySharedMutex;
+
+void ReadData()
+{
+	std::shared_lock<std::shared_mutex> lock(mySharedMutex);
+	// 여러 스레드에서 동시에 읽기 수행
+	std::cout << "Reading data..." << std::endl;
+}
+
+void WriteData()
+{
+	std::unique_lock<std::shared_mutex> lock(mySharedMutex);
+	// 쓰기 작업 수행
+	std::cout << "Writing data..." << std::endl;
+}
+
 int main()
 {
-	for (int32 i = 0; i < 5; ++i)
-	{
-		cout << i << endl;
-	}
-	/*for (int32 i = 0; i < 2; ++i)
-	{
-		GThreadManager->Launch(ThreadWrite);
-	}
+	// 여러 스레드가 동시에 읽고 쓰는 예제
+	std::thread reader1(ReadData);
+	std::thread reader2(ReadData);
+	std::thread writer(WriteData);
 
-	for (int32 i = 0; i < 5; ++i)
-	{
-		GThreadManager->Launch(ThreadRead);
-	}
+	reader1.join();
+	reader2.join();
+	writer.join();
 
-	GThreadManager->Join();*/
+	return 0;
 }
